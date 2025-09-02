eb51878f06450d22ef435bbdf83c91a0299c0113
nowy <zen3chw@gmail.com>
2025-09-01 22:01:24 -0400

    Create custom-combo-maker.py

---
+++ custom-combo-maker.py
@@ -0,0 +1,12 @@
+cons = "abcdefghijklmnopqrstuvwxyz0123456789"
+
+combos = [
+    f"{first}{second}{third}" for first in cons for second in cons
+    for third in cons
+]
+
+with open("username.txt", "w") as file:
+    for combo in combos:
+        file.write(combo + "\n")
+
+print("Combos generated to username.txt")
